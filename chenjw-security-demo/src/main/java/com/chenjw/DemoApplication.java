85e01bf01c22414b83b903582a5c36a4467c9a87
chenjianwei@zxycloud.com <chenjianwei@zxycloud.com>
2020-02-20 23:30:32 +0800

    第一次提交学习记录

---
+++ chenjw-security-demo/src/main/java/com/chenjw/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.chenjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: TODO
+ * @author: 陈建伟
+ * @date: 2020-02-15
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	/**
+	 * @Description: TODO(这里用一句话描述这个方法的作用) 
+	 * @author: 陈建伟
+	 * @date: 2020-02-15
+	 * @param args
+	 * @throws
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ chenjw-security-demo/src/main/java/com/chenjw/controller/DemoFirstController.java
@@ -0,0 +1,13 @@
+package com.chenjw.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoFirstController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello spring security!";
+	}
+}
